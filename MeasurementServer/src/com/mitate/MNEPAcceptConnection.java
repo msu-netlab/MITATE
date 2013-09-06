83d5be5aff5fac8ea29a0b4b05a5b91441450ad4
Utkarsh <utkarsh.goel@cs.montana.edu>
2013-09-05 23:50:13 -0600

    removed irrelavant code

--- MeasurementServer/src/com/mitate/MNEPAcceptConnection.java
+++ MeasurementServer/src/com/mitate/MNEPAcceptConnection.java
@@ -49,15 +49,4 @@ public class MNEPAcceptConnection {
        
     }
     
-    private double distFrom(double lat1, double lng1, double lat2, double lng2) {
-        double earthRadius = 3958.75;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-               Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-               Math.sin(dLng/2) * Math.sin(dLng/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return   earthRadius * c;
-      }
-    
 }
