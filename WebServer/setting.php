26967012b4a0d8bf7dadf0df7deccbb31355c580
Utkarsh <utkarsh.goel@cs.montana.edu>
2013-09-08 17:05:45 -0600

    combined everything

---
+++ WebServer/setting.php
@@ -0,0 +1,23 @@
+<?php include("usermenu.php"); ?>
+<center><p style="font-family:calibri;"><font size=4">Edit your mobile device settings</font></p>
+<br />
+<form action="">
+<table align="center" style="font-family:calibri;" border="1">
+<tr>
+<td align="center">Mobile Device Name</td><td align="center">Poll Interval (min)</td><td align="center">Data Cap (MB/mo)</td><td align="center">Wi-Fi Cap (MB/mo)</td>
+</tr>
+<tr>
+<td align="center">Samsung Galaxy S3</td><td align="center"><input type="text" value="20" /></td><td align="center"><input type="text" value="100" /></td><td align="center"><input type="text" value="500" /></td>
+</tr>
+<tr>
+<td align="center">Google Nexus</td><td align="center"><input type="text" value="10" /></td><td align="center"><input type="text" value="350" /></td><td align="center"><input type="text" value="900" /></td>
+</tr>
+<tr>
+<td align="left"><input type="submit" value="Submit Changes" ></td><td></td><td></td><td></td>
+</tr>
+</table>
+</form>
+<?php include("bmenu.php");?>
+
+
+
