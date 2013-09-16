b7dcb7c63dff757b40c1fb52ff8a90a278fc96e8
Utkarsh <utkarsh.goel@cs.montana.edu>
2013-09-16 14:34:09 -0600

    added query exp API + ,modified sh

---
+++ WebServer/mitate_query_experiment.php
@@ -0,0 +1,57 @@
+<?php
+$dbhostname = "localhost";
+$dbusername = "mitate";
+$dbpassword = "Database4Mitate";
+$dbschemaname = "mitate";
+$dbconnection = mysql_connect($dbhostname, $dbusername, $dbpassword);
+if (!$dbconnection)	{die('Could not connect: ' . mysql_error());}
+mysql_select_db($dbschemaname, $dbconnection);
+$loginresultset = mysql_query("SELECT count(*) as status FROM userinfo where username = '$_POST[username]' and password = '$_POST[password]'");
+if ($loginresultset) {
+    $loginresultrow = mysql_fetch_assoc($loginresultset);
+    if ($loginresultrow['status'] == "1") {
+		$experiment_count = 0;
+		$get_experiment_list = mysql_query("select * from experiment where experiment_id = $_POST[experiment_id] and ((username = '$_POST[username]' and permission = 'private') or permission = 'public')");
+		while($get_experiment = mysql_fetch_assoc($get_experiment_list)) {
+			echo "insert into experiment (experiment_id, username, permission) values($get_experiment[experiment_id], '$get_experiment[username]', '$get_experiment[permission]');";
+			$get_transaction_list = mysql_query("select * from transaction1 where experiment_id = $get_experiment[experiment_id]");
+			while($get_transaction = mysql_fetch_assoc($get_transaction_list)) {
+				echo "insert into transaction1 (transactionid, username, count, original_count, experiment_id) values($get_transaction[transactionid], '$get_transaction[username]', $get_transaction[count], $get_transaction[original_count], $get_transaction[experiment_id]);";
+				$get_criteria_linked_list = mysql_query("select * from trans_criteria_link where transactionid = $get_transaction[transactionid]");
+				while($get_criteria_linked = mysql_fetch_assoc($get_criteria_linked_list)) {
+					echo "insert into trans_criteria_link (criteriaid, transactionid) values ($get_criteria_linked[criteriaid], $get_criteria_linked[transactionid]);";
+					$get_criteria_list = mysql_query("select * from criteria where criteriaid = $get_criteria_linked[criteriaid]");
+					while($get_criteria = mysql_fetch_assoc($get_criteria_list)) {
+						echo "insert into criteria (criteriaid, specification, deviceid) values($get_criteria[criteriaid], '$get_criteria[specification]', '$get_criteria[deviceid]');";
+					}
+				}
+				$get_transfer_linked_list = mysql_query("select * from trans_transfer_link where transactionid = $get_transaction[transactionid]");
+				while($get_transfer_linked = mysql_fetch_assoc($get_transfer_linked_list)) {
+					echo "insert into trans_transfer_link (transferid, transactionid, orderno) values($get_transfer_linked[transferid], $get_transfer_linked[transactionid], $get_transfer_linked[orderno]);";
+					$get_transfer_list = mysql_query("select * from transfer where transferid = $get_transfer_linked[transferid]");
+					while($get_transfer = mysql_fetch_assoc($get_transfer_list)) {
+						echo "insert into transfer (transferid, sourceip, destinationip, bytes, type, transferadded, packetdelay, explicit, content, noofpackets, protocoltype, portnumber, contenttype, response) values ($get_transfer[transferid], '$get_transfer[sourceip]', '$get_transfer[destinationip]', $get_transfer[bytes], $get_transfer[type], '$get_transfer[transferadded]', $get_transfer[packetdelay], $get_transfer[explicit], '$get_transfer[content]', $get_transfer[noofpackets], '$get_transfer[protocoltype]', $get_transfer[portnumber], '$get_transfer[contenttype]', $get_transfer[response]);";
+						$get_metricdata_transfer_list = mysql_query("select * from metricdata where transferid = $get_transfer_linked[transferid]");
+						while($get_metricdata_transfer = mysql_fetch_assoc($get_metricdata_transfer_list)) {
+							echo "insert into metricdata (metricid, transferid, transactionid, value, transferfinished, deviceid) values ($get_metricdata_transfer[metricid], $get_metricdata_transfer[transferid], $get_metricdata_transfer[transactionid], $get_metricdata_transfer[value], '$get_metricdata_transfer[transferfinished]', '$get_metricdata_transfer[deviceid]');";
+						}
+						$get_transferexecutedby_transfer_list = mysql_query("select * from transferexecutedby where transferid = $get_transfer_linked[transferid]");
+						while($get_transferexecutedby_transfer = mysql_fetch_assoc($get_transferexecutedby_transfer_list)) {
+							echo "insert into transferexecutedby (transferid, devicename, username, carriername, deviceid) values ($get_transferexecutedby_transfer[transferid], '$get_transferexecutedby_transfer[devicename]', '$get_transferexecutedby_transfer[username]', '$get_transferexecutedby_transfer[carriername]', '$get_transferexecutedby_transfer[deviceid]');";
+						}
+					}
+				}
+				$get_transaction_fetched_list = mysql_query("select * from transaction_fetched where transactionid = $get_transaction[transactionid]");
+				while($get_transaction_fetched = mysql_fetch_assoc($get_transaction_fetched_list)) {
+					echo "insert into transaction_fetched (transactionid, deviceid) values ($get_transaction_fetched[transactionid], '$get_transaction_fetched[deviceid]'); ";
+				}
+			}
+			$experiment_count = $experiment_count + 1;
+		}
+		if($experiment_count == 0)
+			echo "Permission denied";
+	}
+	else
+		echo "Invalid account credentials.";
+}
+?>
--- WebServer/sample/mitate.sh
+++ WebServer/sample/mitate.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-echo "Enter your MITATE username: ";
-read username;
-echo "Enter your password: ";
-read password;
+#echo "Enter your MITATE username: ";
+#read username;
+#echo "Enter your password: ";
+#read password;
 if [ "$1" == 'upload' ] 
 then
-	curl -k -ssl3 -F "username=$username" -F "password=$password" -F file=@$2 https://mitate.cs.montana.edu/mitate_upload_experiment.php
+	curl -k -ssl3 -F "username=test" -F "password=test" -F file=@$2 https://mitate.cs.montana.edu/mitate_upload_experiment.php
 elif [ "$1" == 'delete' ] 
 then
-	curl -k -ssl3 -F "username=$username" -F "password=$password" -F experiment_id=$2 https://mitate.cs.montana.edu/mitate_delete_experiment.php
+	curl -k -ssl3 -F "username=test" -F "password=test" -F experiment_id=$2 https://mitate.cs.montana.edu/mitate_delete_experiment.php
+elif [ "$1" == 'query' ]
+then
+	curl -k -ssl3 -F "username=test" -F "password=test" -F experiment_id=$2 https://mitate.cs.montana.edu/mitate_query_experiment.php
+else
+	echo "Missing operation";
 fi
