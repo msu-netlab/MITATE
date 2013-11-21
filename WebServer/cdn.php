f92f74103516247700d503f354ada7fe58e36f91
Utkarsh <utkarsh.goel@cs.montana.edu>
2013-11-21 15:42:41 -0700

    added data retrieval for logs table

--- WebServer/cdn.php
+++ WebServer/cdn.php
@@ -7,6 +7,13 @@
 	mysql_select_db("mitate", $con);
 	$k = 0;
 	$time = str_replace("T", " ", $_GET[time]);
+	if($_GET[oneway] == 0 && $_GET[size] == 0 && $_GET[rtt] == 0) {
+		$sql="insert into logs (username, transferid, deviceid, logmessage) values('$_GET[username]', $_GET[transferid], '$_GET[deviceid]', '$_GET[log]')";
+		if (!mysql_query($sql,$con)) {
+			die('Error: ' . mysql_error());
+		}
+	}
+	else {
 	$sql="INSERT INTO metricdata (metricid, transferid, transactionid, value, transferfinished, deviceid) VALUES(10027, $_GET[transferid], $_GET[transactionid], $_GET[oneway], '$time', '$_GET[deviceid]')";
 	if (!mysql_query($sql,$con))
 	{
@@ -29,15 +36,7 @@
 	}
 	else $k = $k +1;
 	
-	/*
-	$sql="update metricdata set value = 1 where metricid = 9999 and transferid = $_GET[transferid] and transactionid = $_GET[transactionid]";
-	if (!mysql_query($sql,$con))
-	{
-	die('Error: ' . mysql_error());
-	}
-	else $k = $k +1;
-	*/
-	
+
 	$sql="update metricdata set transferfinished = '$time' where transferid = $_GET[transferid] and transactionid = $_GET[transactionid] and deviceid = '$_GET[deviceid]'";
 	if (!mysql_query($sql,$con))
 	{
@@ -52,11 +51,16 @@
 	}
 	else $k = $k +1;
 	
-	if($k==5)
-	echo "1";
+	if($k==5) {
+		$sql="INSERT INTO metricdata (metricid, transferid, transactionid, transferfinished, deviceid, responsedata) VALUES(10040, $_GET[transferid], $_GET[transactionid], '$time', '$_GET[deviceid]', '$_GET[log]')";
+		if (!mysql_query($sql, $con)) {
+			die('Error: ' . mysql_error());
+		}
+		echo "1";
+	}
 	else
-	echo "0";
-	
+		echo "0";
+	}
 	mysqli_close($con);
 
 ?>
