4ec9eaf59e3c1b8f04d451b72a6bb48ff6cf826c
Utkarsh <utkarsh.goel@cs.montana.edu>
2014-03-04 03:00:18 -0700

    added settings to web interface

--- WebServer/header.php
+++ WebServer/header.php
@@ -10,7 +10,7 @@
 	<hr />
 	<div style="background-color:white;height:auto;font-size: 18px;text-align: center;"><a href="index.php" style="text-decoration:none;">Home</a> | <a href="mitate_publications.php" style="text-decoration:none;">Publications</a> | <a href="mitate_signup.php" style="text-decoration:none;">Signup</a> | <a href="https://github.com/msu-netlab/MITATE/" style="text-decoration:none;" target="_blank">Code</a> | <a href="MITATE_Tutorial_For_Beginners.php" style="text-decoration:none;">Tutorial</a> | <a href="mitate_downloads.php" style="text-decoration:none;">Downloads</a> | <a style="text-decoration:none;" href="mailto:mitate@cs.montana.edu">Contact Us</a> | <a style="text-decoration:none;" href="mitate_team.php">Team</a>
 	<?php if(isset($_SESSION['mitateLoggedInUser'])) { ?>
-	| <a style="color: black;text-decoration:none;" href="mitate_logout.php">Logout</a>
+	| <a style="color: black;text-decoration:none;" href="settings.php">Settings</a> | <a style="color: black;text-decoration:none;" href="mitate_logout.php">Logout</a>
 	<?php } ?>
 	</div>
 	<hr />
---
+++ WebServer/settings.php
@@ -0,0 +1,99 @@
+<?php session_start();
+include('header.php');
+$con = mysql_connect("localhost","mitate","Database4Mitate");
+if (!$con) {
+	die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("mitate", $con);
+if($_POST[cellular_credits] !='' && $_POST[wifi_credits] != '') {
+	$sql="update usercredits set available_cellular_credits = $_POST[cellular_credits], available_wifi_credits = $_POST[wifi_credits] where username = '$_SESSION[mitateLoggedInUser]'";
+	if (!mysql_query($sql,$con)) {
+		die('Error: ' . mysql_error());
+	}
+}
+	
+$tempcount=0;
+while($tempcount < $_POST['rowCount']) {
+	$tempcount = $tempcount + 1;
+	$tempdeviceid = $_POST["deviceid" . $tempcount] ;
+	$pollintervalvar = $_POST["pollinterval" . $tempdeviceid];
+	$minbatterypowervar = $_POST["minbatterypower" . $tempdeviceid];
+	if($minbatterypowervar != '' && $pollintervalvar != '') {
+		$sql="update userdevice set pollinterval = $pollintervalvar, minbatterypower = $minbatterypowervar where deviceid = $tempdeviceid";
+		if (!mysql_query($sql,$con)) {
+			die('Error: ' . mysql_error());
+		}
+	}
+}
+
+$getUserSettings = mysql_query("SELECT * from usercredits where username='$_SESSION[mitateLoggedInUser]';");
+while($userSettingRow = mysql_fetch_array($getUserSettings))
+  {
+?>
+<h3 style="text-decoration:underline">Account Settings:</h3>
+<form action="" method="POST">
+<table>
+<tr>
+<td style="font-size: 18;text-align: justify;">Available Cellular Credits (MB)</td>
+<td style="margin:20px;">
+<input style="font-size: 18;" type="text" name="cellular_credits" value="<?php echo $userSettingRow[available_cellular_credits];?>" />
+</td>
+</tr>
+<tr>
+<td style="font-size: 18;text-align: justify;">Available Wi-Fi Credits (MB)</td>
+<td style="margin:20px;">
+<input style="font-size: 18;" type="text" name="wifi_credits" value="<?php echo $userSettingRow[available_wifi_credits];?>" />
+</td>
+</tr>
+<tr>
+<td></td><td><input style="font-size: 18;" type="submit" value="Update" /></td>
+</tr>
+</table>
+</form>
+<br />
+<h3 style="text-decoration:underline">Device Settings:</h3>
+
+<?php
+}
+$deviceCount = 0;
+$getUserDeviceCount = mysql_query("SELECT count(*) as count FROM userdevice where username='$_SESSION[mitateLoggedInUser]';");
+while($deviceCounts = mysql_fetch_array($getUserDeviceCount)) {
+	$deviceCount = $deviceCounts[count];
+}
+if($deviceCount > 0) {
+	?>
+		<form action="" method="POST">
+	<table>
+	<th style="font-size: 18;">Device Name</th><th style="font-size: 18;">Poll Interval (mins)</th><th style="font-size: 18;">Min. Battery (%)</th>
+	<?php
+	$getUserDeviceSettings = mysql_query("SELECT username, devicename, deviceid, pollinterval, minbatterypower FROM userdevice where username='$_SESSION[mitateLoggedInUser]';");
+	$rowCount=0;
+	while($deviceSettingRow = mysql_fetch_array($getUserDeviceSettings)) {
+		$rowCount = $rowCount + 1;	
+?>	
+		<tr>
+		<input type="hidden" value="<?php echo $deviceSettingRow['deviceid']; ?>" name="deviceid<?php echo $rowCount; ?>" />
+		<td>
+		<label style="font-size: 18;"><?php echo $deviceSettingRow['devicename']; ?></label>
+		</td>
+		<td>
+		<input style="font-size: 18;" type="text" value="<?php echo $deviceSettingRow['pollinterval']; ?>" name="pollinterval<?php echo $deviceSettingRow['deviceid']; ?>" />
+		</td>
+		<td>
+		<input style="font-size: 18;" type="text" value="<?php echo $deviceSettingRow['minbatterypower']; ?>" name="minbatterypower<?php echo $deviceSettingRow['deviceid']; ?>" />
+		</td>
+		</tr>
+<?php
+	}
+	?>
+	<tr><td></td><td><input style="font-size: 18;" type="submit" value="Update" /></td></tr>
+	<tr>
+	<input type="hidden" value="<?php echo $rowCount; ?>" name="rowCount" />
+	</tr>
+	</table>
+		</form>
+<?php
+}
+else
+	echo '<label style="font-size: 18;">You do not have any registered devices.</label>';
+?>
