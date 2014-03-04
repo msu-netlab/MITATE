38988c0ead82e05422aef32cfb0de89cfac3f276
Utkarsh <utkarsh.goel@cs.montana.edu>
2014-03-04 00:07:53 -0700

    removed

--- WebServer/sample/tcp_uplink_jitter.sql
+++
@@ -1,8 +0,0 @@
-select md.value as max_tcp_uplink_jitter 
-from experiment exp, transactions trans, trans_transfer_link ttl, metric mt, metricdata md 
-where exp.experiment_id = 1000000005 -- replace the experiment ID with the experiment ID you got in step 16
-and trans.experiment_id = exp.experiment_id 
-and ttl.transactionid = trans.transactionid 
-and ttl.transferid = md.transferid 
-and md.metricid = mt.metricid 
-and mt.name = 'tcp_maximum_uplink_jitter' 
