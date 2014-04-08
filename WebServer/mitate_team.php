1a9f09dc55af0ef9e5b87739ab3e0b41f6f64731
Utkarsh <utkarsh.goel@cs.montana.edu>
2014-04-08 13:20:09 -0600

    added photos of team member on website

--- WebServer/mitate_team.php
+++ WebServer/mitate_team.php
@@ -1,13 +1,45 @@
 <?php include('header.php'); ?>
-	<div style="font-size: 18;text-align: justify;">
-	<h3 style="text-decoration:underline">Faculty Involved:</h3>
-	1. <a href="http://www.cs.montana.edu/mwittie/" target="_blank">Prof. Mike P. Wittie</a>
-	<br />
-	2. <a href="http://www.cs.montana.edu/yang/" target="_blank">Prof. Qing Yang</a>
-	<br />
-	<h3 style="text-decoration:underline">Students Involved:</h3>
-	1. <a href="http://www.cs.montana.edu/~utkarsh.goel/" target="_blank">Utkarsh Goel</a>
-	<br />
-	2. Ajay Miyyapuram
+<br />
+	<div style="font-size: 18;text-align: justify;margin:0 auto;width:800px;">
+	<a target="_blank" href="http://www.cs.montana.edu/~utkarsh.goel/" style="text-decoration: none;margin-left:20px;">
+		<div style="display:inline-block;">
+			<div>
+				<img src="img/utkarsh-goel.jpg" style="height:200px;width:160px;border:solid black 1px;">
+			</div>
+			<div style="text-align: center;width: 160px;">
+				<span style="font-weight: bold;">Utkarsh Goel</span><br /><span>Ph.D. Student</span>
+			</div>
+		</div>
+	</a>
+	<a target="_blank" href="https://www.linkedin.com/in/ajaymiyyapuram" style="text-decoration: none;margin-left:40px;">
+		<div style="display:inline-block;">
+			<div>
+				<img src="img/ajay-miyyapuram.png" style="height:200px;width:160px;border:solid black 1px;">
+			</div>
+			<div style="text-align: center;width: 160px;">
+				<span style="font-weight: bold;">Ajay Miyyapuram</span><br /><span>MS Student</span>
+			</div>
+		</div>
+	</a>
+	<a target="_blank" href="http://www.cs.montana.edu/mwittie/" style="text-decoration: none;margin-left:40px;">
+		<div style="display:inline-block;">
+			<div>
+				<img src="img/mike-wittie.jpg" style="height:200px;width:160px;border:solid black 1px;">
+			</div>
+			<div style="text-align: center;width: 160px;">
+				<span style="font-weight: bold;">Mike Wittie</span><br /><span>Professor</span>
+			</div>
+		</div>
+	</a>
+	<a target="_blank" href="http://www.cs.montana.edu/yang/" style="text-decoration: none;margin-left:40px;">
+		<div style="display:inline-block;">
+			<div>
+				<img src="img/qing-yang.jpg" style="height:200px;width:160px;border:solid black 1px;">
+			</div>
+			<div style="text-align: center;width: 160px;">
+				<span style="font-weight: bold;">Qing Yang</span><br /><span>Professor</span>
+			</div>
+		</div>
+	</a>
 	</div>
 <?php include('footer.php'); ?>
