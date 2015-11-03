975ccd4cdd714c60ca56d2db660ef1f283bd40cf
Utkarsh Goel <utkarsh.090@gmail.com>
2015-11-03 13:10:38 -0700

    removed visitor script

--- WebServer/footer.php
+++ WebServer/footer.php
@@ -5,6 +5,5 @@
 <br />
 </div>
 </div>
-<div style="display:none;" id="clustrmaps-widget"></div><script type="text/javascript">var _clustrmaps = {'url' : 'http://mitate.cs.montana.edu/', 'user' : 1137670, 'server' : '2', 'id' : 'clustrmaps-widget', 'version' : 1, 'date' : '2014-03-26', 'lang' : 'en', 'corners' : 'square' };(function (){ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'http://www2.clustrmaps.com/counter/map.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);})();</script><noscript><img src="http://www2.clustrmaps.com/stats/maps-no_clusters/mitate.cs.montana.edu--thumb.jpg" alt="Locations of visitors to this page" /></noscript>
 </body>
 </html>
