c075d13a2bba80868c5ae4da68b46e5ca013e060
Brad White <white.brad17@gmail.com>
2017-03-08 11:28:03 -0700

    Refactoring to BQ

--- WebServer/get_experiment_status.php
+++ WebServer/get_experiment_status.php
@@ -8,20 +8,24 @@ if (!$bigQuery) {
 $encrypted_password = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($passwordEncryptionKey), $_POST[password], MCRYPT_MODE_CBC, md5(md5($passwordEncryptionKey))));
 $loginresultset = $bigQuery->runQuery("SELECT COUNT(*) AS status FROM MITATE.userinfo WHERE username = '$_POST[username]' AND password = '$encrypted_password' AND status = 1");
 $queryInfo = $loginresultset->info();
-if ($info['totalRows'] > 0) {
-    $loginresultrow = mysql_fetch_assoc($loginresultset);
-    if ($loginresultrow['status'] == "1") {
-        $get_status_list = $bigQuery->runQuery("SELECT COUNT(teb.transferid) AS totalExecuted, COUNT(ttl.transferid) AS totalTransfers
-		FROM MITATE.experiment exp, MITATE.transactions tr, MITATE.trans_transfer_link ttl LEFT OUTER JOIN MITATE.transferexecutedby teb ON ttl.transferid = teb.transferid
-		WHERE exp.experiment_id = $_POST[experiment_id]
-		AND exp.username = '$_POST[username]'
-		AND exp.experiment_id = tr.experiment_id
-		AND tr.transactionid = ttl.transactionid;");
-        while ($get_status = mysql_fetch_assoc($get_status_list)) {
-            if ($get_status[totalTransfers] != 0)
-                echo 'Total number of transfer(s): ' . $get_status[totalTransfers] . '\nTotal number of transfer(s) executed: ' . $get_status[totalExecuted] . '\nTo get more details, run mitate.sh query <outfile>';
-            else
-                echo 'You do not have permissions to get the status of this experiment.';
+if ($queryInfo['totalRows'] > 0) {
+    foreach ($loginresultset->rows() as $loginresultrow) {
+        if ($loginresultrow['status'] == "1") {
+            $get_status_list = $bigQuery->runQuery("SELECT COUNT(teb.transferid) AS totalExecuted, 
+                                                    COUNT(ttl.transferid) AS totalTransfers
+		                                            FROM MITATE.experiment exp, MITATE.transactions tr, MITATE.trans_transfer_link ttl 
+		                                            LEFT OUTER JOIN MITATE.transferexecutedby teb 
+		                                            ON ttl.transferid = teb.transferid
+		                                            WHERE exp.experiment_id = $_POST[experiment_id]
+		                                            AND exp.username = '$_POST[username]'
+		                                            AND exp.experiment_id = tr.experiment_id
+		                                            AND tr.transactionid = ttl.transactionid;");
+            foreach ($get_status_list->rows() as $get_status) {
+                if ($get_status['totalTransfers'] != 0)
+                    echo 'Total number of transfer(s): ' . $get_status['totalTransfers'] . '\nTotal number of transfer(s) executed: ' . $get_status['totalExecuted'] . '\nTo get more details, run mitate.sh query <outfile>';
+                else
+                    echo 'You do not have permissions to get the status of this experiment.';
+            }
         }
     }
 }
--- WebServer/get_user_credits.php
+++ WebServer/get_user_credits.php
@@ -1,29 +1,26 @@
 <?php
-libxml_use_internal_errors(true); 
+libxml_use_internal_errors(true);
 $xml = simplexml_load_file("config.xml");
-$dbhostname = $xml->databaseConnection->serverAddress;
-$dbusername = $xml->databaseConnection->user;
-$dbpassword = $xml->databaseConnection->password;
-$dbschemaname = $xml->databaseConnection->name;
-$passwordEncryptionKey = $xml->database->passwordEncryptionKey;
-$con = mysql_connect($dbhostname, $dbusername, $dbpassword);
-if (!$con)
-{
-	die('Could not connect: ' . mysql_error());
+list($bigQuery, $dataset) = require 'get_bq_connection.php';
+if (!$bigQuery) {
+    die('Could not connect to BigQuery');
 }
-mysql_select_db($dbschemaname, $con);
 $username = $_POST[username];
 $password = $_POST[password];
 $encrypted_password = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($passwordEncryptionKey), $password, MCRYPT_MODE_CBC, md5(md5($passwordEncryptionKey))));
-$loginresultset = mysql_query("SELECT count(*) as status FROM userinfo where username = '$username' and password = '$encrypted_password' and status = 1");
-if ($loginresultset) {
-	$loginresultrow = mysql_fetch_assoc($loginresultset);
-    if ($loginresultrow['status'] == 1) {	
-		$check_user_credits = mysql_query("select available_cellular_credits, contributed_cellular_credits, available_wifi_credits, contributed_wifi_credits from usercredits where username = '$username'");
-		$check_user_credit = mysql_fetch_assoc($check_user_credits);
-		echo 'Total available cellular credits: ' . $check_user_credit[available_cellular_credits] . ' MB\n Total contributed cellular credits: ' . $check_user_credit[contributed_cellular_credits] . ' MB\n Total available Wi-Fi credits: ' . $check_user_credit[available_wifi_credits] . ' MB\n Total contributed Wi-Fi credits: ' . $check_user_credit[contributed_wifi_credits] . ' MB';
-	}
-	else
-		echo "Invalid login";
+$loginresultset = $bigQuery->runQuery("SELECT COUNT(*) AS status FROM MITATE.userinfo WHERE username = '$username' AND password = '$encrypted_password' AND status = 1");
+$queryInfo = $loginresultset->info();
+if ($queryInfo['totalRows'] > 0) {
+    foreach ($loginresultset->rows() as $loginresultrow) {
+        if ($loginresultrow['status'] == "1") {
+            $check_user_credits = $bigQuery->runQuery("SELECT available_cellular_credits, contributed_cellular_credits, available_wifi_credits, contributed_wifi_credits 
+                                                       FROM MITATE.usercredits 
+                                                       WHERE username = '$username'");
+            foreach ($check_user_credits->rows() as $check_user_credit) {
+                echo 'Total available cellular credits: ' . $check_user_credit['available_cellular_credits'] . ' MB\n Total contributed cellular credits: ' . $check_user_credit['contributed_cellular_credits'] . ' MB\n Total available Wi-Fi credits: ' . $check_user_credit['available_wifi_credits'] . ' MB\n Total contributed Wi-Fi credits: ' . $check_user_credit['contributed_wifi_credits'] . ' MB';
+            }
+        } else
+            echo "Invalid login";
+    }
 }
 ?>
